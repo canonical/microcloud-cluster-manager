3c4cbb14c0b6c420a36603f39ace1d9cd4fe50e3
Mason Hu <mason.hu@canonical.com>
2024-06-05 13:10:28 +0200

    chore: add static code analysis for backend to CI
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- api/types/site.go
+++ api/types/site.go
@@ -3,7 +3,7 @@ package types
 
 // Site is a standalone or clustered LXD site.
 type Site struct {
-	Name      string    `json:"name"`
-	Addresses []string  `json:"addresses"`
-	Status    string    `json:"status"`
+	Name      string   `json:"name"`
+	Addresses []string `json:"addresses"`
+	Status    string   `json:"status"`
 }
