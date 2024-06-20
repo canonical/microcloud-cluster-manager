09d25af361a611636b5605911a68b02e582cc858
Mason Hu <mason.hu@canonical.com>
2024-06-20 11:47:52 +0200

    assign site management listener to a variable
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- internal/api/server.go
+++ internal/api/server.go
@@ -6,28 +6,29 @@ import (
 	"github.com/canonical/lxd-site-manager/internal/api/types"
 )
 
-// Servers contains all the network listeners for site manager.
-var Servers = []rest.Server{
-	// site management listener (same network listener as core)
-	{
-		CoreAPI: true,
-		Resources: []rest.Resources{
-			{
-				PathPrefix: types.NoPrefix,
-				Endpoints: []rest.Endpoint{
-					uiRootCmd,
-					uiCmd,
-					uiAssetsCmd,
-					uiImgCmd,
-				},
+var siteManagementListener = rest.Server{
+	CoreAPI: true,
+	Resources: []rest.Resources{
+		{
+			PathPrefix: types.NoPrefix,
+			Endpoints: []rest.Endpoint{
+				uiRootCmd,
+				uiCmd,
+				uiAssetsCmd,
+				uiImgCmd,
 			},
-			{
-				PathPrefix: types.ApiVersionPrefix,
-				Endpoints: []rest.Endpoint{
-					siteCmd,
-					sitesCmd,
-				},
+		},
+		{
+			PathPrefix: types.APIVersionPrefix,
+			Endpoints: []rest.Endpoint{
+				siteCmd,
+				sitesCmd,
 			},
 		},
 	},
+}
+
+// Servers contains all the network listeners for site manager.
+var Servers = []rest.Server{
+	siteManagementListener,
 }
--- internal/api/types/server.go
+++ internal/api/types/server.go
@@ -3,8 +3,8 @@ package types
 import "github.com/canonical/microcluster/rest/types"
 
 const (
-	// ApiVersionPrefix is the path prefix for API related endpoints.
-	ApiVersionPrefix types.EndpointPrefix = "1.0"
+	// APIVersionPrefix is the path prefix for API related endpoints.
+	APIVersionPrefix types.EndpointPrefix = "1.0"
 	// NoPrefix is the path prefix for any endpoints that should be located at server root.
 	NoPrefix types.EndpointPrefix = ""
 )
