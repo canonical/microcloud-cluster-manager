bd70c33aff40591d579ef97f0faa3059d92fe4e7
David Edler <david.edler@canonical.com>
2024-06-25 16:24:16 +0200

    fix: ui auth should redirect to login on auth errors and to site list after user authenticated. add a 404 page.
    Signed-off-by: David Edler <david.edler@canonical.com>

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,16 +1,46 @@
-import { FC, lazy, Suspense } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import React, { FC, lazy, Suspense } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { queryKeys } from "util/queryKeys";
+import { fetchSites } from "api/sites";
+import NoMatch from "pages/NoMatch";
 
 const SiteList = lazy(() => import("pages/sites/SiteList"));
 const Login = lazy(() => import("pages/Login"));
 
 const App: FC = () => {
+  const { pathname } = useLocation();
+
+  const { error, isLoading } = useQuery({
+    queryKey: [queryKeys.sites],
+    queryFn: fetchSites,
+    retry: false,
+  });
+
+  const isAuthError = error?.message === "not authorized";
+  const isLoginPath = pathname === "/ui/login";
+
+  if (!isLoading && !isLoginPath && isAuthError) {
+    window.location.href = "/ui/login";
+    return null;
+  }
+
+  if (!isLoading && isLoginPath && !isAuthError) {
+    window.location.href = "/ui/sites";
+    return null;
+  }
+
   return (
     <Suspense fallback={<div>Loading</div>}>
       <Routes>
-        <Route path="/" element={<Navigate to="/ui" replace={true} />} />
-        <Route path="/ui" element={<Login />} />
+        <Route path="/" element={<Navigate to="/ui/sites" replace={true} />} />
+        <Route
+          path="/ui"
+          element={<Navigate to="/ui/sites" replace={true} />}
+        />
+        <Route path="/ui/login" element={<Login />} />
         <Route path="/ui/sites" element={<SiteList />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </Suspense>
   );
