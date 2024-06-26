a3082966b6900b1be9c84423c65e8a2233ae4924
Mason Hu <mason.hu@canonical.com>
2024-06-26 14:50:49 +0200

    setup database schema and helper functions
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

---
+++ internal/database/site_details.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"time"
+)
+
+//go:generate -command mapper lxd-generate db mapper -t site_details.mapper.go
+//go:generate mapper reset
+//
+//go:generate mapper stmt -d github.com/canonical/microcluster/cluster -e site_detail objects table=site_details
+//go:generate mapper stmt -d github.com/canonical/microcluster/cluster -e site_detail objects-by-Status table=site_details
+//go:generate mapper stmt -d github.com/canonical/microcluster/cluster -e site_detail objects-by-CoreSiteID table=site_details
+//go:generate mapper stmt -d github.com/canonical/microcluster/cluster -e site_detail id table=site_details
+//go:generate mapper stmt -d github.com/canonical/microcluster/cluster -e site_detail create table=site_details
+//go:generate mapper stmt -d github.com/canonical/microcluster/cluster -e site_detail update table=site_details
+//
+//go:generate mapper method -i -d github.com/canonical/microcluster/cluster -e site_detail GetMany
+//go:generate mapper method -i -d github.com/canonical/microcluster/cluster -e site_detail GetOne
+//go:generate mapper method -i -d github.com/canonical/microcluster/cluster -e site_detail ID
+//go:generate mapper method -i -d github.com/canonical/microcluster/cluster -e site_detail Exists
+//go:generate mapper method -i -d github.com/canonical/microcluster/cluster -e site_detail Create
+//go:generate mapper method -i -d github.com/canonical/microcluster/cluster -e site_detail Update
+
+// SiteDetail represents all site level data.
+type SiteDetail struct {
+	CoreSiteID       int    `db:"primary=true"`
+	Status           string `db:"primary=true"`
+	ID               int
+	InstanceCount    int
+	InstanceStatuses string
+	JoinedAt         time.Time
+	UpdatedAt        time.Time
+}
+
+// SiteDetailFilter is a required struct for use with lxd-generate. It is used for filtering fields on database fetches.
+type SiteDetailFilter struct {
+	Status     *string
+	CoreSiteID *int
+}
