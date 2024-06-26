5137872ac9fc48d94b0abb40b7b2f86422282878
Mason Hu <mason.hu@canonical.com>
2024-06-26 15:23:36 +0200

    add dedicated dummy site data generator
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- cmd/lxd-site-mgrd/main.go
+++ cmd/lxd-site-mgrd/main.go
@@ -2,14 +2,11 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"os"
 
 	"github.com/canonical/lxd/shared/logger"
 	"github.com/canonical/microcluster/config"
 	"github.com/canonical/microcluster/microcluster"
-	"github.com/canonical/microcluster/state"
 	"github.com/spf13/cobra"
 
 	"github.com/canonical/lxd-site-manager/internal/api"
@@ -77,37 +74,7 @@ func (c *cmdDaemon) run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	hooks := &config.Hooks{
-		PostBootstrap: func(s *state.State, initConfig map[string]string) error {
-			return s.Database.Transaction(context.TODO(), func(ctx context.Context, tx *sql.Tx) error {
-				testStmt := `
-					INSERT INTO core_sites (name, site_certificate) VALUES ('site1', 'a');
-					INSERT INTO core_sites (name, site_certificate) VALUES ('site2', 'as');
-					INSERT INTO core_sites (name, site_certificate) VALUES ('site3', 'asd');
-					INSERT INTO site_details (core_site_id, status, instance_statuses) VALUES (1, 'PENDING_APPROVAL', '{}');
-					INSERT INTO site_details (core_site_id, status, instance_statuses) VALUES (2, 'ACTIVE', '{}');
-					INSERT INTO site_details (core_site_id, status, instance_statuses) VALUES (3, 'ACTIVE', '{}');
-					INSERT INTO site_member_statuses (core_site_id, member_name, address, architecture, role, usage_cpu, usage_memory, usage_disk, status) 
-						VALUES (1, 'member1', '127.0.0.1:9001', 'x86_64', 'controller', 0.0, 0.0, 0.0, 'ACTIVE');
-					INSERT INTO site_member_statuses (core_site_id, member_name, address, architecture, role, usage_cpu, usage_memory, usage_disk, status) 
-						VALUES (1, 'member2', '127.0.0.1:9002', 'x86_64', 'controller', 0.0, 0.0, 0.0, 'ACTIVE');
-					INSERT INTO site_member_statuses (core_site_id, member_name, address, architecture, role, usage_cpu, usage_memory, usage_disk, status) 
-						VALUES (2, 'member1', '127.0.0.1:9001', 'x86_64', 'controller', 0.0, 0.0, 0.0, 'ACTIVE');
-					INSERT INTO site_member_statuses (core_site_id, member_name, address, architecture, role, usage_cpu, usage_memory, usage_disk, status) 
-						VALUES (2, 'member2', '127.0.0.1:9002', 'x86_64', 'controller', 0.0, 0.0, 0.0, 'ACTIVE');
-					INSERT INTO site_member_statuses (core_site_id, member_name, address, architecture, role, usage_cpu, usage_memory, usage_disk, status) 
-						VALUES (3, 'member1', '127.0.0.1:9001', 'x86_64', 'controller', 0.0, 0.0, 0.0, 'ACTIVE');
-					INSERT INTO site_member_statuses (core_site_id, member_name, address, architecture, role, usage_cpu, usage_memory, usage_disk, status) 
-						VALUES (3, 'member2', '127.0.0.1:9002', 'x86_64', 'controller', 0.0, 0.0, 0.0, 'ACTIVE');
-					
-				`
-				_, err := tx.ExecContext(ctx, testStmt)
-				return err
-			})
-		},
-	}
-
-	return m.Start(cmd.Context(), database.SchemaExtensions, api.Extensions(), hooks)
+	return m.Start(cmd.Context(), database.SchemaExtensions, api.Extensions(), &config.Hooks{})
 }
 
 func main() {
