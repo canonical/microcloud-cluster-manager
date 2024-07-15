3097b176bee66bbfac3cfd71cc0e5a214d0092a4
Mason Hu <mason.hu@canonical.com>
2024-07-15 10:34:53 +0200

    tokens: cli for adding external site join tokens
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

---
+++ cmd/lxd-site-mgr/external_site_join_tokens.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"time"
+
+	"github.com/canonical/microcluster/microcluster"
+	"github.com/spf13/cobra"
+
+	"github.com/canonical/lxd-site-manager/internal/api/types"
+	"github.com/canonical/lxd-site-manager/internal/client"
+	"github.com/canonical/lxd-site-manager/version"
+)
+
+type cmdExternalSiteJoinToken struct {
+	common *CmdControl
+}
+
+func (c *cmdExternalSiteJoinToken) command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "external-site-join-token",
+		Short: "manage external join tokens for LXD sites.",
+		RunE:  c.run,
+	}
+
+	var cmdAdd = cmdExternalSiteJoinTokenAdd{common: c.common}
+	cmd.AddCommand(cmdAdd.command())
+
+	return cmd
+}
+
+func (c *cmdExternalSiteJoinToken) run(cmd *cobra.Command, args []string) error {
+	return cmd.Help()
+}
+
+type cmdExternalSiteJoinTokenAdd struct {
+	common *CmdControl
+
+	flagExpiry time.Duration
+}
+
+func (c *cmdExternalSiteJoinTokenAdd) command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "add <site_name>",
+		Short: "Add a new external join token for a LXD site.",
+		Example: `
+			lxd-site-mgr external-site-join-token add <site_name>
+			Will created an external site join token for a LXD site.
+		`,
+		RunE: c.run,
+	}
+
+	cmd.Flags().DurationVar(&c.flagExpiry, "expiry", 0, "Specify the duration (i.e. 5m or 48h) for the token to be valid")
+
+	return cmd
+}
+
+func (c *cmdExternalSiteJoinTokenAdd) run(cmd *cobra.Command, args []string) error {
+	m, err := microcluster.App(microcluster.Args{
+		StateDir: c.common.FlagStateDir,
+		Verbose:  c.common.FlagLogVerbose,
+		Debug:    c.common.FlagLogDebug,
+		Version:  version.Version(),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	cli, err := m.LocalClient()
+	if err != nil {
+		return err
+	}
+
+	if len(args) < 1 {
+		return cmd.Help()
+	}
+
+	// convert expiry flag from duration to time stamp
+	expiry := time.Time{}
+	if c.flagExpiry != 0 {
+		expiry = time.Now().Add(c.flagExpiry)
+	}
+
+	siteName := args[0]
+	payload := types.ExternalSiteTokenPost{
+		SiteName: siteName,
+		Expiry:   expiry,
+	}
+
+	token, err := client.ExternalSiteJoinTokenPostCmd(cmd.Context(), cli, &payload)
+	if err != nil {
+		return err
+	}
+
+	cmd.Println("The token can not be retrieved at a later stage, please save it now.")
+	cmd.Println(token)
+	return nil
+}
---
+++ internal/client/external_site_join_tokens.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/canonical/lxd/shared/api"
+	"github.com/canonical/microcluster/client"
+
+	"github.com/canonical/lxd-site-manager/internal/api/types"
+)
+
+// ExternalSiteJoinTokenPostCmd sends a POST request to /1.0/external-site-join-token.
+func ExternalSiteJoinTokenPostCmd(ctx context.Context, c *client.Client, payload *types.ExternalSiteTokenPost) (token string, err error) {
+	queryCtx, cancel := context.WithTimeout(ctx, time.Second*30)
+	defer cancel()
+
+	tokenResponse := &types.ExternalSiteTokenPostResponse{}
+	url := api.NewURL().Path("external-site-join-token")
+	err = c.Query(queryCtx, "POST", types.APIVersionPrefix, url, payload, tokenResponse)
+	if err != nil {
+		clientURL := c.URL()
+		return "", fmt.Errorf("Failed performing action on %q: %w", clientURL.String(), err)
+	}
+
+	return tokenResponse.Token, nil
+}
