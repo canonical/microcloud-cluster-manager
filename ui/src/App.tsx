7c5308558b0885a3c7ef050620527c24098bb2b5
Mason Hu <mason.hu@canonical.com>
2024-07-23 18:10:30 +0200

    fix: avoid flicker when loading login page
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,43 +1,47 @@
-import React, { FC, lazy, Suspense } from "react";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { FC, lazy, Suspense } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import NoMatch from "pages/NoMatch";
 import Settings from "pages/Settings";
 import { useAuth } from "context/auth";
 import CreateCluster from "pages/clusters/CreateCluster";
+import { logout } from "util/helpers";
 
 const ClusterList = lazy(() => import("pages/clusters/ClusterList"));
 const Login = lazy(() => import("pages/Login"));
 
 const App: FC = () => {
-  const { pathname } = useLocation();
   const { isAuthLoading, isAuthenticated } = useAuth();
-  const isLoginPath = pathname === "/ui/login";
 
-  if (!isAuthLoading && !isLoginPath && !isAuthenticated) {
-    window.location.href = "/ui/login";
-    return null;
+  const preLoginRoutes = (
+    <>
+      <Route path="/ui/login" element={<Login />} />
+      <Route path="*" element={<Navigate to="/ui/login" replace={true} />} />
+    </>
+  );
+
+  const loggedInRoutes = (
+    <>
+      <Route path="/" element={<Navigate to="/ui/sites" replace={true} />} />
+      <Route path="/ui" element={<Navigate to="/ui/sites" replace={true} />} />
+      <Route path="/ui/sites" element={<ClusterList />} />
+      <Route path="/ui/sites/:activeTab" element={<ClusterList />} />
+      <Route path="/ui/sites/create" element={<CreateCluster />} />
+      <Route path="/ui/settings" element={<Settings />} />
+      <Route path="*" element={<NoMatch />} />
+    </>
+  );
+
+  if (isAuthLoading) {
+    return;
   }
 
-  if (!isAuthLoading && isLoginPath && isAuthenticated) {
-    window.location.href = "/ui/sites";
-    return null;
+  if (!isAuthenticated) {
+    logout();
   }
 
   return (
-    <Suspense fallback={<div>Loading</div>}>
-      <Routes>
-        <Route path="/" element={<Navigate to="/ui/sites" replace={true} />} />
-        <Route
-          path="/ui"
-          element={<Navigate to="/ui/sites" replace={true} />}
-        />
-        <Route path="/ui/login" element={<Login />} />
-        <Route path="/ui/sites" element={<ClusterList />} />
-        <Route path="/ui/sites/:activeTab" element={<ClusterList />} />
-        <Route path="/ui/sites/create" element={<CreateCluster />} />
-        <Route path="/ui/settings" element={<Settings />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+    <Suspense>
+      <Routes>{isAuthenticated ? loggedInRoutes : preLoginRoutes}</Routes>
     </Suspense>
   );
 };
