4464304ed5ea3df2148ba55566b783edf1d9e38c
Mason Hu <mason.hu@canonical.com>
2024-12-06 07:50:07 +0200

    chore: setup github action for PR checks
    Signed-off-by: Mason Hu <mason.hu@canonical.com>

--- deployment/rock/build.sh
+++ deployment/rock/build.sh
@@ -2,20 +2,19 @@
 
 # The script requires:
 # - rockcraft
-# - yq
 # - docker
 
 set -e
 
 rockcraft pack -v
 
-VERSION=$(awk -F': ' '/^version:/ {print $2}' rockcraft.yaml | tr -d '"')
-ROCK="lxd-cluster-manager_${VERSION}_amd64.rock"
+# Use the value of IMAGE if set, otherwise call `make docker-image-name`
+# Locally, $IMAGE may be set by skaffold for custom artifact builders
+IMAGE=${IMAGE:-$(make docker-image-name)}
 
-# $IMAGE is env var set by skaffold for custom artifact builders
 rockcraft.skopeo --insecure-policy copy \
-    oci-archive:$ROCK \
-    docker-daemon:$IMAGE
+    oci-archive:$(make rock-name) \
+    docker-daemon:$IMAGE --debug
 
 # remove rock after copying over to docker daemon
-rm $ROCK
+rm $(make rock-name)
